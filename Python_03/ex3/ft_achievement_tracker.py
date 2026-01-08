bced2e53106750237d21e88f9b7adce1af0a63de
Mousaab Laktaoui <mlaktaou@e1r8p4.1337.ma>
2026-01-08 21:58:45 +0100

    push

--- Python_03/ex3/ft_achievement_tracker.py
+++ Python_03/ex3/ft_achievement_tracker.py
@@ -1,5 +1,5 @@
 data = {'alice': ['first_blood', 'pixel_perfect', 'speed_runner', 'first_blood', 'first_blood'],
-        'bob': ['level_master', 'boss_hunter', 'treasure_seeker', 'level_master', 'level_master'],
+        'bob': ['level_master', 'boss_hunter', 'treasure_seeker', 'level_master', 'level_master', 'first_blood'],
         'charlie': ['treasure_seeker', 'boss_hunter', 'combo_king', 'first_blood', 'boss_hunter', 'first_blood', 'boss_hunter', 'first_blood'],
         'diana': ['first_blood', 'combo_king', 'level_master', 'treasure_seeker', 'speed_runner', 'combo_king', 'combo_king', 'level_master'],
         'eve': ['level_master', 'treasure_seeker', 'first_blood', 'treasure_seeker', 'first_blood', 'treasure_seeker'],
@@ -43,13 +43,21 @@ def achievement_analytics(data):
     print(f"Rare achievements (1 player): {difference_achievements}")
 
 def two_players_common(data, player1, player2):
-    print("")
+    common = set(data[player1]).intersection(set(data[player2]))
+    player1_unique = set(data[player1]).difference(set(data[player2]))
+    player2_unique = set(data[player2]).difference(set(data[player1]))
+    if not common:
+        common = "{}"
+    if not player1_unique:
+        player1_unique = "{}"
+    if not player2_unique:
+        player2_unique = "{}"
+    print(f"{player1.capitalize()} vs {player2.capitalize()} common: {common}")
     
 #data_players_show(data)
-achievement_analytics(data)
+two_players_common(data, 'alice', 'bob')
 
-
-
+def hhh():
     alice_bob_common = (set(players_data['alice']['achievements']) &
                         set(players_data['bob']['achievements']))
 
